95e55425c103c7b2c07df1462e6fd2d0243930ab
C-Collamar <christian.collamar@gmail.com>
2016-12-17 15:01:27 +0800

    Update macros.h

--- Line Drawing/macros.h
+++ Line Drawing/macros.h
@@ -4,4 +4,7 @@
 #define RESTART 1 //numeric representation for restarting the animation
 #define TERMINATE 2 //numeric representation for terminating the animation
 
+#define MARGIN 10 //defines the number of intention pixels from the side of the pattern to be rendered
 #define UNIT 50 //the number of square units/boxes to be drawn
+
+#define DELAY 50 //time interval before rendering the next line(in milliseconds)
