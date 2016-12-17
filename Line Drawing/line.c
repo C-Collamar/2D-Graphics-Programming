7366a19bdf0373b4ee0024fb6e34d1ea66acca7f
C-Collamar <christian.collamar@gmail.com>
2016-12-17 14:59:21 +0800

    Update line.c

--- Line Drawing/line.c
+++ Line Drawing/line.c
@@ -22,19 +22,28 @@ int main(int argc, char *argv[]) {
 	SDL_Event event; //used for reading events in the event queue
 	Line point; //this line object will be manipulated by adjusting its end-point coordinates, and rendered into the renderer
 
-	int xPixels = (WIDTH / UNIT); //defines the number of horizontal pixels to be added to/subtracted from the current x-coordinate of a point
-	int yPixels = (WIDTH / UNIT); //defines the number of vertical pixels to be added to/subtracted from the current y-coordinate of a point
-
 	//mark the center point
 	int center[2] = {WIDTH / 2, HEIGHT / 2};
 
-	//mark the beginning of the animation
-	//this is probably one of the few instances where goto is advantageous
-	BEGIN:
+	int xPixels = (WIDTH - MARGIN) / UNIT; //defines the number of horizontal pixels to be added to/subtracted from the current x-coordinate of a point
+	int yPixels = (HEIGHT - MARGIN) / UNIT; //defines the number of vertical pixels to be added to/subtracted from the current y-coordinate of a point
+
+	if(xPixels < yPixels) {
+		 yPixels = xPixels;
+		 xPixels = yPixels;
+	}
+	else {
+		xPixels = yPixels;
+		yPixels = xPixels;
+	}
 
 	//set the background color to {199, 0, 57, 255}
 	SDL_SetRenderDrawColor(renderer, 199, 0, 57, 255);
 	SDL_RenderClear(renderer);
+
+	//mark the beginning of the animation
+	//this is probably one of the few scenarios where goto is advantageous
+	BEGIN:
 
 	//initially set the drawing color to white
 	SDL_RenderPresent(renderer);
@@ -44,13 +53,13 @@ int main(int argc, char *argv[]) {
 	while(1) {
 		//set the initial position of the line to vertical, centered
 		point.A[0] = center[0];
-		point.A[1] = xPixels;
+		point.A[1] = HEIGHT - (yPixels * UNIT);
 
 		point.B[0] = center[0];
 		point.B[1] = center[1];
 
 		//FOR QUADRANT 1:
-		while(point.A[1] + yPixels <= point.B[1]) {
+		while(point.A[1] < center[1]) {
 			//check if user wants to restart the animation from the beginning or terminate the program
 			switch(userRequest(&event)) {
 				case RESTART:
@@ -58,19 +67,19 @@ int main(int argc, char *argv[]) {
 				case TERMINATE:
 					goto END;
 			}
+
+			//display the line in its updated position
+			drawLine(&renderer, point.A, point.B);
+			SDL_RenderPresent(renderer);
+			SDL_Delay(DELAY);
 
 			//Change the position of the line accordingly
 			point.A[1] += yPixels;
 			point.B[0] += xPixels;
-
-			//display the line in its updated position
-			drawLine(&renderer, point.A, point.B);
-			SDL_RenderPresent(renderer);
-			SDL_Delay(100);
 		}
 
 		//FOR QUADRANT 4:
-		while(point.B[0] - xPixels > center[0]) {
+		while(point.B[0] > center[0]) {
 			//check if user wants to restart the animation from the beginning or terminate the program
 			switch(userRequest(&event)) {
 				case RESTART:
@@ -78,19 +87,19 @@ int main(int argc, char *argv[]) {
 				case TERMINATE:
 					goto END;
 			}
+
+			//display the line in its updated position
+			drawLine(&renderer, point.A, point.B);
+			SDL_RenderPresent(renderer);
+			SDL_Delay(DELAY);
 
 			//Change the position of the line accordingly
 			point.B[0] -= xPixels;
 			point.A[1] += yPixels;
-
-			//display the line in its updated position
-			drawLine(&renderer, point.A, point.B);
-			SDL_RenderPresent(renderer);
-			SDL_Delay(100);
 		}
 
 		//FOR QUADRANT 3:
-		while(point.A[1] - yPixels > center[1]) {
+		while(point.A[1] - 5 > center[1]) {
 			//check if user wants to restart the animation from the beginning or terminate the program
 			switch(userRequest(&event)) {
 				case RESTART:
@@ -99,14 +108,14 @@ int main(int argc, char *argv[]) {
 					goto END;
 			}
 
-			//Change the position of the line accordingly
-			point.B[0] -= xPixels;
-			point.A[1] -= yPixels;
-
 			//display the line in its updated position
 			drawLine(&renderer, point.A, point.B);
 			SDL_RenderPresent(renderer);
-			SDL_Delay(100);
+			SDL_Delay(DELAY);
+
+			//Change the position of the line accordingly
+			point.B[0] -= xPixels;
+			point.A[1] -= yPixels;
 		}
 
 		//FOR QUADRANT 2:
@@ -119,14 +128,14 @@ int main(int argc, char *argv[]) {
 					goto END;
 			}
 
-			//Change the position of the line accordingly
-			point.B[0] += xPixels;
-			point.A[1] -= yPixels;
-
 			//display the line in its updated position
 			drawLine(&renderer, point.A, point.B);
 			SDL_RenderPresent(renderer);
-			SDL_Delay(100);
+			SDL_Delay(DELAY);
+
+			//Change the position of the line accordingly
+			point.B[0] += xPixels;
+			point.A[1] -= yPixels;
 		}
 
 		//set the drawing color to a randomly generated color
